e20f866600ccf61e00762625de97dc393fe3d660
willianDotNet <williandotnet@outlook.com>
2024-12-03 11:58:29 -0300

    Melhorias e Inclusão de Teste Unitário
    Melhoria na organização da program
    Refatorando com SOLID
    Incluindo regra de desconto

---
+++ Vendas.WebApi/Configs/EventDispatcherConfig.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation.AspNetCore;
+using FluentValidation;
+using Vendas.Domain.Validators;
+using Vendas.Data.Events;
+using Vendas.Domain.Interfaces.Events;
+
+namespace Vendas.WebApi.Configs
+{
+    public static class EventDispatcherConfig
+    {
+        public static WebApplicationBuilder AddEventDispatcherConfig(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSingleton<IEventDispatcher>(sp =>
+            {
+                var logger = sp.GetRequiredService<Microsoft.Extensions.Logging.ILogger<IEventDispatcher>>();
+                bool useRabbitMq = builder.Configuration.GetValue<bool>("UseRabbitMq");
+                return new EventDispatcher(logger, useRabbitMq);
+            });
+
+            return builder;
+        }
+    }
+}
